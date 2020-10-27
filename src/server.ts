fffe4865016d7a22d1e9108c620c215c5485fbfb
Carlos <crodrigsantos@gmail.com>
2020-10-27 02:19:14 -0300

    Api criada com sucesso

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,21 @@
 import express from 'express';
+import cors from 'cors'; //executar api em diferentes domínios
+import path from 'path';
+import 'express-async-errors'; //capturar erros de validação e execução
 import './database/connection';
+import errorHandler from './errors/handler';
 import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json()); //permite o uso de JSON nas requisições HTTP
 app.use(routes);
 
-app.listen(3333, ()=> {
-    console.log('Servidor rodando na porta 3333!');
-});
+//                                              diretório do arquivo .. ..
+app.use('/uploads', express.static(path.join(__dirname, '..', 'src', 'uploads')));
+
+
+app.use(errorHandler)
+
+app.listen(3333);
